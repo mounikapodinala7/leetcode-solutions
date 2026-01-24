a0132973bd65c7ec4e4cdede02cc0edda554bb99
Mounika Podinala <mounikapodinala03@gmail.com>
2026-01-24 22:05:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.50%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -3,14 +3,8 @@ class Solution(object):
         """
         :type s: str
         :rtype: str
-        """
-        # Split the string into words
-        words = s.split()
-        
-        # Reverse the list of words
-        reversed_words = words[::-1]
-        
-        # Join the reversed words with spaces
-        reversed_string = ' '.join(reversed_words)
-        
+        """        
+        words = s.split()                
+        reversed_words = words[::-1]        
+        reversed_string = ' '.join(reversed_words)        
         return reversed_string
