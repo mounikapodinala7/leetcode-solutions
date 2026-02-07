f8fea6b9d8b3641145f95e8ea386d37044799abc
Mounika Podinala <mounikapodinala03@gmail.com>
2026-02-07 20:52:58 +0530

    Time: 3 ms (58.44%) | Memory: 20.5 MB (26.40%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -1,21 +1,23 @@
 class Solution:
     def generateTrees(self, n: int):
-        def generate_trees(start, end):
+        if n == 0:
+            return []
+
+        def build(start, end):
             if start > end:
-                return [None,]
-            
-            all_trees = []
+                return [None]
+
+            trees = []
             for i in range(start, end + 1):
-                left_trees = generate_trees(start, i - 1)
-                right_trees = generate_trees(i + 1, end)
-                
-                for l in left_trees:
-                    for r in right_trees:
-                        current_tree = TreeNode(i)
-                        current_tree.left = l
-                        current_tree.right = r
-                        all_trees.append(current_tree)
-            
-            return all_trees
-        
-        return generate_trees(1, n) if n else []
+                left = build(start, i - 1)
+                right = build(i + 1, end)
+
+                for l in left:
+                    for r in right:
+                        root = TreeNode(i)
+                        root.left = l
+                        root.right = r
+                        trees.append(root)
+            return trees
+
+        return build(1, n)
