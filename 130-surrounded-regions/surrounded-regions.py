9b773fe6b871118c83da0c1b8d6ec95db6c18160
Mounika Podinala <mounikapodinala03@gmail.com>
2026-02-08 20:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (40.81%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,29 +1,36 @@
+from typing import List
+
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        n = len(board)
-        m = len(board[0])
-        vis = [[0] * m for _ in range(n)]
+        if not board or not board[0]:
+            return
 
-        def dfs(x, y):
-            vis[x][y] = 1
-            for dx, dy in [(-1,0), (0,1), (1,0), (0,-1)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < n and 0 <= ny < m and not vis[nx][ny] and board[nx][ny] == 'O':
-                    dfs(nx, ny)
+        m, n = len(board), len(board[0])
 
-        for i in range(n):
-            if board[i][0] == 'O':
-                dfs(i, 0)
-            if board[i][m-1] == 'O':
-                dfs(i, m-1)
+        def dfs(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n or board[r][c] != 'O':
+                return
 
-        for j in range(m):
-            if board[0][j] == 'O':
-                dfs(0, j)
-            if board[n-1][j] == 'O':
-                dfs(n-1, j)
+            board[r][c] = 'S'  # mark as safe
 
-        for i in range(n):
-            for j in range(m):
-                if board[i][j] == 'O' and vis[i][j] == 0:
-                    board[i][j] = 'X'
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        # 1. Run DFS from all border 'O's
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n - 1)
+
+        for j in range(n):
+            dfs(0, j)
+            dfs(m - 1, j)
+
+        # 2. Flip captured regions and restore safe ones
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'   # captured
+                elif board[i][j] == 'S':
+                    board[i][j] = 'O'   # restore
