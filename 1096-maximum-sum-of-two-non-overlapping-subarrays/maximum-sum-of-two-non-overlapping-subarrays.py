98b257ea34947b6f4ef1f865534d84aa4101a932
Mounika Podinala <mounikapodinala03@gmail.com>
2026-02-09 21:19:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (16.22%) - LeetSync

--- 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.py
@@ -1,16 +1,24 @@
 class Solution:
-    def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:
-
-        nums = list(accumulate(nums, initial = 0))
-        mx1 = mx2 = mx3 = 0
+    def maxSumTwoNoOverlap(self, nums, L, M):
+        n = len(nums)
         
-        for sm0,sm1,sm2,sm3 in zip(nums, 
-                                   nums[firstLen:],
-                                   nums[secondLen:],
-                                   nums[firstLen+secondLen:]):
-
-            mx1 = max(mx1, sm1 - sm0)
-            mx2 = max(mx2, sm2 - sm0)
-            mx3 = max(mx3, max(mx1 + sm3-sm1, mx2 + sm3-sm2))
-            
-        return mx3
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+        
+        def subarray_sum(i, length):
+            return prefix[i + length] - prefix[i]
+        
+        ans = 0
+        
+        maxL = 0
+        for i in range(L, n - M + 1):
+            maxL = max(maxL, subarray_sum(i - L, L))
+            ans = max(ans, maxL + subarray_sum(i, M))
+        
+        maxM = 0
+        for i in range(M, n - L + 1):
+            maxM = max(maxM, subarray_sum(i - M, M))
+            ans = max(ans, maxM + subarray_sum(i, L))
+        
+        return ans
