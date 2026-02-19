887622a48815cf56f4b250833643a5b6ed3b6d1e
Mounika Podinala <mounikapodinala03@gmail.com>
2026-02-19 22:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (36.94%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -5,7 +5,6 @@ public:
         int i = a.size() - 1, j = b.size() - 1;
         int k = res.size() - 1;
         int C = 0;
-
         while (k >= 0) {
             int A = i < 0 ? 0 : a[i] & 1; // '0' & 1 = 0, '1' & 1 = 1
             int B = j < 0 ? 0 : b[j] & 1; //
@@ -13,12 +12,10 @@ public:
             int S = (A ^ B) ^ C; 
             int C_out = ((A ^ B) & C) | (A & B);
 
-            res[k] = S + '0'; //char conversion
-
+            res[k] = S + '0'; 
             C = C_out;
-            i--; j--; k--; //decrement pointers
+            i--; j--; k--; 
         }
-
         return C ? (res.insert(res.begin(), '1'), res) : res;
     }
 };
